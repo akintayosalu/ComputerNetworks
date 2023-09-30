050c3165be6a10d38b6aabf0d32cde9b28f7b2d4
Akintayo Salu <saluakintayo@gmail.com>
2023-09-29 20:53:35 -0400

    wifitransmitter and design file completed and submitted

--- hw1/wifireceiver.py
+++ hw1/wifireceiver.py
@@ -68,6 +68,7 @@ def hard_viterbi_decoding(encoded_bits):
     transition2input = {0:0, 1:1, 2:0, 3:1, 4:0, 5:1, 6:0, 7:1}
     node2trainsition = {0:[0,1], 1:[2,3], 2:[4,5], 3:[6,7]}
     
+    #calculate branch weights for decoding 
     for i in range(result_length):
         received_bits = encoded_bits[2*i:2*i+2]
         for stateI in range(num_of_states):
@@ -77,9 +78,7 @@ def hard_viterbi_decoding(encoded_bits):
                 transitionI = 2*stateI + idx
                 branch_metrics[i][transitionI] = branch_metric
 
-    minHeap = []
-    heapq.heapify(minHeap)
-    info = []
+    info = [] #store minpath for all nodes
     for t in range(result_length+1):
         currentState = []
         for i in range(num_of_states):
@@ -87,7 +86,7 @@ def hard_viterbi_decoding(encoded_bits):
         info.append(currentState)
 
     info[0][0][0] = 0
-    
+    #find minimum path for all nodes using a variation of Dijkstra's Algorithm
     for t in range(result_length): #do not need to do calculations for last set of nodes 
         nodeRange = 4
         if t == 0: nodeRange = 1
@@ -112,6 +111,7 @@ def hard_viterbi_decoding(encoded_bits):
     minTransition = info[idx][minNode][1][2]
     input_bits.append(transition2input[minTransition])
 
+    #traverse backwards using the transitions to find input bits using minimum path 
     while idx > 1:
         idx -= 1
         prevNode = minTransition//2
@@ -121,6 +121,7 @@ def hard_viterbi_decoding(encoded_bits):
     input_bits = input_bits[::-1]
     return input_bits
 
+#this helper function demodulates a set of values
 def demodulated(transmitted_output):
     nfft = 64
     mod = comm.modulation.QAMModem(4)
@@ -132,6 +133,7 @@ def demodulated(transmitted_output):
     new_output = np.concatenate((length_bits, decoded_bits))
     return preamble_bits, new_output
 
+#this helper function fourier transforms a set of values 
 def fourier_transform(transmitted_output):
     nfft = 64
     nsym = int(len(transmitted_output)/nfft)
@@ -140,6 +142,32 @@ def fourier_transform(transmitted_output):
         transmitted_output[i*nfft:(i+1)*nfft] = np.fft.fft(symbol)
 
     return transmitted_output
+
+def preamble_detection(transmitted_output):
+    #idenfify the most likely starting position of preamble
+    nfft = 64
+    preamble = np.array([1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1,1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1])
+    mod = comm.modulation.QAMModem(4)
+    modulated_preamble = np.fft.ifft(mod.modulate(preamble.astype(bool)))
+    correlation = np.correlate(transmitted_output,modulated_preamble)
+    noise_pad_begin_length = np.argmax(correlation)
+
+    #grab the encoded bits with the zero padding at the end 
+    transformed_output = fourier_transform(transmitted_output[noise_pad_begin_length:])
+    demodulated_bits = mod.demodulate(transformed_output, demod_type='hard')
+    encoded_bits_with_padding = demodulated_bits[4*nfft:] 
+    length_bits = demodulated_bits[2*nfft:4*nfft]
+    preamble_bits = demodulated_bits[:2*nfft]
+        
+    encoded_length = np.reshape(np.trim_zeros(length_bits),[-1,3])
+    decoded_length = np.apply_along_axis(binary_decode, axis=1, arr=encoded_length) #decode length bits
+    length = int(''.join(map(lambda x: str(int(x)), decoded_length)), 2)
+
+    #remove the zero padding at the end of the signal  
+    num_of_added_zeros = 2*nfft-(length*8)%(2*nfft)
+    message_bit_size = (length*8) + num_of_added_zeros
+    encoded_bits = encoded_bits_with_padding[:message_bit_size*2]
+    return encoded_bits,length_bits,noise_pad_begin_length
 
 def WifiReceiver(output, level):
     transmitted_output = output
@@ -153,33 +181,8 @@ def WifiReceiver(output, level):
         preamble, new_output = demodulated(transmitted_output)
         return de_interleaving(new_output)
     elif level == 4:
-        nfft = 64
-        preamble = np.array([1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1,1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1])
-        mod = comm.modulation.QAMModem(4)
-        modulated_preamble = np.fft.ifft(mod.modulate(preamble.astype(bool)))
-        correlation = np.correlate(transmitted_output,modulated_preamble)
-        noise_pad_begin_length = np.argmax(correlation)
-
-        transformed_output = fourier_transform(transmitted_output[noise_pad_begin_length:])
-        demodulated_bits = mod.demodulate(transformed_output, demod_type='hard')
-        encoded_bits_with_padding = demodulated_bits[4*nfft:] 
-        length_bits = demodulated_bits[2*nfft:4*nfft]
-        preamble_bits = demodulated_bits[:2*nfft]
-        
-        encoded_length = np.reshape(np.trim_zeros(length_bits),[-1,3])
-        decoded_length = np.apply_along_axis(binary_decode, axis=1, arr=encoded_length) #decode length bits
-        length = int(''.join(map(lambda x: str(int(x)), decoded_length)), 2)
-        
-        num_of_added_zeros = 2*nfft-(length*8)%(2*nfft)
-        message_bit_size = (length*8) + num_of_added_zeros
-        encoded_bits = encoded_bits_with_padding[:message_bit_size*2]
+        encoded_bits,length_bits,noise_pad_begin_length = preamble_detection(transmitted_output)
         decoded_bits = hard_viterbi_decoding(encoded_bits)
         new_output = np.concatenate((length_bits, decoded_bits))
         _, message, message_length = de_interleaving(new_output)
-        
         return noise_pad_begin_length,message, message_length
-
-#noise_pad_begin, txsignal, length = WifiTransmitter("my name is akintayo", 4)
-#begin_zero_padding, message, message_length = WifiReceiver(txsignal,4)
-
-#print(begin_zero_padding, message, message_length)
