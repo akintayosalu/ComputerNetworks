e162635177da481e428501c44c5d5d3ce37c8fdb
Akintayo Salu <saluakintayo@gmail.com>
2023-10-26 15:00:18 -0400

    client and server seem to communicate effectively

--- hw2/content_server.py
+++ hw2/content_server.py
@@ -2,41 +2,80 @@ import sys
 import ast
 import socket, sys
 import threading
-
+import json 
 BUFSIZE = 1024  # size of receiving buffer
 
 node_info = dict() #stores info like uuid, name, port, peer_count
 node_neighbors = dict()
 present_neighbors = dict()
 
+count = 0
+lock = threading.Lock()
+print_lock = threading.Lock()
+
+
 def keep_alive():
-    for n in node_neighbors:
-        server_address = node_neighbors[n]["hostname"]
-        server_port = int(node_neighbors[n]["backend_port"])
-        # create socket
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.setblocking(0) #make non-blocking 
-        # generate message
-        msg_string = node_info["uuid"] + " -> " + node_neighbors[n]["uuid"]
+    while True:
+        copy_neighbors = json.dumps(node_neighbors)
+        neigh_data = json.loads(copy_neighbors)
+        for n in neigh_data:
+            data = neigh_data[str(n)]
+            server_address = data["hostname"]
+            server_port = int(data["backend_port"])
 
-        # send message to server
-        s.sendto(msg_string.encode(), (server_address, server_port))
+            # create socket
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        try:
-            # get echo message
-            echo_string, addr = s.recvfrom(BUFSIZE)
-            # print echo message
-            print('Echo from the server: '+echo_string.decode(),flush=True)
-        except socket.error as e:
-            # If no data is available, an error will be raised
-            pass
+            node_msg = {"name":node_info["name"], "uuid": node_info["uuid"], "backend_port": node_info["backend_port"], "distance_metric" : int(data["distance_metric"])}
+            msg_string = "sendKA "+ "|" + str(node_msg) +  "|"  + " -> " + "|" + data["uuid"] 
 
-        s.close() #close port 
+            # send message to server
+            s.sendto(msg_string.encode(), (server_address, server_port))
+            s.close() #close port 
 
 def client():
     while True:
-        keep_alive()
+        server_address = "localhost"
+        server_port = int(node_info["backend_port"])
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        # get message from keyboard
+        #msg_string = input('Send this to the server: ')
+        msg_string = input()
+
+        # send message to server
+        s.sendto(msg_string.encode(), (server_address, server_port))
+        s.close()
     #sys.exit(0) dont really want to exit program
+
+def send_ack(msg):
+    _, info, _, _ = msg.split("|")
+    neigh_info = ast.literal_eval(info)
+    name, uuid, hostname, backend_port, distance_metric = neigh_info["name"], neigh_info["uuid"], "localhost", neigh_info["backend_port"], neigh_info["distance_metric"]
+    global count
+    with lock:
+        neigh_info = {"uuid": uuid, 
+                                "hostname" : hostname,
+                                "backend_port" : backend_port, 
+                                "distance_metric": distance_metric,
+                                    "name": name,
+                                    "active": True}
+        node_neighbors[count] = neigh_info
+        present_neighbors[uuid] = count
+        count += 1
+
+    server_address = "localhost"
+    server_port = backend_port
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # send acknowledgement msg
+    msg_string = "confirmKA " + node_info["uuid"] + " " + node_info["name"]
+
+    # send message to server
+    s.sendto(msg_string.encode(), (server_address, int(server_port)))
+    s.close()
+
+
 
 def receive():
     # create socket
@@ -44,17 +83,31 @@ def receive():
     SERVER_PORT = int(node_info["backend_port"])
     s.bind(("localhost", SERVER_PORT))
 
+    keep_alive_messages = threading.Thread(target=keep_alive, daemon=True)
+    keep_alive_messages.start()
+
     # main loop
     while True:
         # accept a packet
-        dgram, addr = s.recvfrom(BUFSIZE)
-        dgram = dgram.decode()
+        msg, addr = s.recvfrom(BUFSIZE)
+        msg = msg.decode()
+        # print(msg)
 
-        # print the message
-        print('New connection from '+str(addr[0])+':'+str(addr[1])+'; Message: '+dgram,flush=True)
-
-        # echo the message back
-        s.sendto(dgram.encode(), addr)
+        if msg == "kill": #not working how I would like 
+            s.close()
+            exit()
+        elif msg == "uuid":
+            output = str({"uuid":node_info["uuid"]})
+            print(ast.literal_eval(output))
+        elif msg == "neighbors":
+            return_neighbors()
+        elif "sendKA" in msg:
+            send_ack(msg)
+        elif "confirmKA" in msg:
+            neigh_uuid = msg.split()[1]
+            idx = present_neighbors[neigh_uuid] 
+            node_neighbors[idx]["name"] = msg.split()[2]
+            node_neighbors[idx]["active"] = True
 
     # exit
     s.close()
@@ -77,24 +130,33 @@ def set_configuration(config_file):
             else:
                 #add mapping of temp 
                 #handling info on neighbor to node 
-                uuid, hostname, backend_port, distance_metric = value.split(",")
-                node_neighbors[key] = {"uuid": uuid.strip(), 
-                                       "hostname" : hostname.strip(),
-                                        "backend_port" : backend_port.strip(), 
-                                         "distance_metric": distance_metric.strip(),
-                                          "name": None,
-                                           "active": False}
+                global count
+                with lock:
+                    uuid, hostname, backend_port, distance_metric = value.split(",")
+                    info = {"uuid": uuid.strip(), 
+                                        "hostname" : "localhost",
+                                            "backend_port" : backend_port.strip(), 
+                                            "distance_metric": distance_metric.strip(),
+                                            "name": None,
+                                            "active": False}
+
+                    node_neighbors[count] = info
+                    present_neighbors[uuid.strip()] = count
+                    count += 1
 
 def return_neighbors():
     output = {"neighbors": dict()}
-    for n in node_neighbors:
-        if node_neighbors[n]["active"]:
-            output["neighbors"][node_neighbors[n]["name"]] = {"uuid": node_neighbors[n]["uuid"], 
-                                                              "host": node_neighbors[n]["hostname"],
-                                                              "backend_port": int(node_neighbors[n]["backend_port"]),
-                                                              "metric": int(node_neighbors[n]["distance_metric"])}
+    copy_neighbors = json.dumps(node_neighbors)
+    neigh_data = json.loads(copy_neighbors)
+    for n in neigh_data:
+        data = neigh_data[str(n)]
+        if data["active"]:
+            output["neighbors"][data["name"]] = {"uuid": data["uuid"], 
+                                                    "host": data["hostname"],
+                                                        "backend_port": int(data["backend_port"]),
+                                                            "metric": int(data["distance_metric"])}
     output = str(output)
-    print(ast.literal_eval(output),flush=True)
+    print(ast.literal_eval(output))
 
 if __name__ == '__main__':
     command = sys.argv[1]
@@ -110,15 +172,3 @@ if __name__ == '__main__':
 
     while True:
         continue
-
-    # for line in sys.stdin:
-    #     print(sys.stdin)
-            
-    #     if "uuid" == line.strip():
-    #         output = str({"uuid":node_info["uuid"]})
-    #         print(ast.literal_eval(output))
-    #     elif "neighbors" == line.strip():
-    #         return_neighbors()
-
-    #     if "Exit" == line.strip():
-    #         break
